f6606a20caf8d1595749f6d66ebe7a815436732d
siavash9000 <siavash@siavash-ThinkPad-X230.(none)>
2014-06-19 20:35:26 +0200

    added implementation of cached matrix inversion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 'cacheSolve' computes and caches the inverse of a given matrix. 
+## It must be used with 'makeCacheMatrix' which creates a list 
+## containing all functions needed for caching.
 
-## Write a short comment describing this function
 
+
+## 'makeCacheMatrix' creates a list containing functions for 
+## caching the inverse of a matrix. 
+##Use 'set' to set the matrix which should be inverted.
+##Use 'get' to get the matrix
+##Use 'setinv' to cache the inverted matrix
+##Use 'getinv' to get the cached invreted matrix (or NULL in case of empty cache)
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve' checks if the given input is already processed and cached.
+## if yes it returns the cached value, else it computes the value and caches it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
